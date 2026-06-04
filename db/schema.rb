08ecd96f3b123c313c946dc8eb95f80115b0bda8
Jorge <jlimajorge@gmail.com>
2026-06-04 08:14:02 -0300

    removendo a tabela customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_01_151328) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_31_131754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-
-  create_table "customers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "email"
-    t.string "name"
-    t.string "phone"
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "description"
-    t.string "title"
-    t.datetime "updated_at", null: false
-  end
 
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
--- spec/factories/customers.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :customer do
-    name { "MyString" }
-    email { "MyString" }
-    phone { "MyString" }
-  end
-end
