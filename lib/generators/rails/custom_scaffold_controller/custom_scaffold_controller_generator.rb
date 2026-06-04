612a76aca08433527966c370c38b3dd71edb8dbc
Jorge <jlimajorge@gmail.com>
2026-06-04 10:46:58 -0300

    atualizando os geradores para camada de controllers

--- lib/generators/rails/custom_scaffold_controller/custom_scaffold_controller_generator.rb
+++ lib/generators/rails/custom_scaffold_controller/custom_scaffold_controller_generator.rb
@@ -7,10 +7,14 @@ module Rails
     class CustomScaffoldControllerGenerator < ScaffoldControllerGenerator
       source_root ScaffoldControllerGenerator.source_root
 
+      def self.source_paths
+        [ Rails.root.join("lib/templates/rails/scaffold_controller").to_s ] + super
+      end
+
       def manage_use_cases
         klass = Rails::Generators.find_by_namespace(:use_case)
-        klass.start([class_name, "Create"], behavior: behavior, destination_root: destination_root)
-        klass.start([class_name, "Update"], behavior: behavior, destination_root: destination_root)
+        klass.start([ class_name, "Create" ], behavior: behavior, destination_root: destination_root)
+        klass.start([ class_name, "Update" ], behavior: behavior, destination_root: destination_root)
       end
     end
   end
